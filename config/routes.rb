961be61dcbf85696a1a6b847061161c73f9a2f32
laipmv-1193 <pham.mai.van.lai@sun-asterisk.com>
2021-11-24 13:22:15 +0700

    Cart Update Quantity

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
     root "static_pages#index"
     get "/home", to: "static_pages#index"
 
-    resources :carts, only: [:index]
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     get "/logout", to: "sessions#destroy"
+
+    resources :carts, only: [:index, :create, :update, :destroy]
   end
 end
